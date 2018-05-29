efbb9dff4ae0e969df0c263f0efa65757f13b5f2
Jovana Djakovic <jovana.djakovic.1995@gmail.com>
2018-05-29 12:40:44 +0200

    Added layouts

---
+++ app/src/main/java/com/singidunum/moviesinfoapp/FiltersActivity.java
@@ -0,0 +1,13 @@
+package com.singidunum.moviesinfoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FiltersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
